aa79cfe056aba166b869880f2ec5a666d1ef360a
Sasha Petrenko <sap625@umsystem.edu>
2022-12-08 15:35:42 -0600

    Update serverpi.sh

--- serverpi.sh
+++ serverpi.sh
@@ -4,8 +4,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/AP6YC/installers/main/arch
 # Install npm then homebridge
 sudo pacman -Sy --needed nodejs-lts-gallium npm make gcc net-tools
 sudo npm install -g --unsafe-perm homebridge homebridge-config-ui-x
+sudo hb-service install --user homebridge
 
 # Install pihole
-git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
-cd "Pi-hole/automated install/"
-sudo bash basic-install.sh
+# git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+# cd "Pi-hole/automated install/"
+# sudo bash basic-install.sh
